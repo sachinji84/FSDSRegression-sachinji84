e4748bdacc53548c6b01854602bd54b857cac65c
sachin5136 <ksachin5136@gmail.com>
2023-04-17 00:02:37 +0530

    FSDSRegression fork commit from my repo

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -19,5 +19,14 @@ if __name__=='__main__':
     model_trainer.initate_model_training(train_arr,test_arr)
 
 
+# Python Command to exeute it in GIT terminal is ;
+# DELL@SachinKPC MINGW64 /d/Mytech/IDE_workspace/ineuron_git_repo/FSDSRegression-sachin-repofolder/FSDSRegression-sachin (main)
+# $ python D:\\Mytech\\IDE_workspace\\ineuron_git_repo\\FSDSRegression-sachin-repofolder\\FSDSRegression-sachin\\src\\pipeline\\training_pipeline.py
+# {'LinearRegression': 0.9302873217953068, 'Lasso': 0.9347652476618369, 'Ridge': 0.9321366737740069, 'Elasticnet': 0.8614823172075561}
 
+# ====================================================================================
+
+# Best Model Found , Model Name : Lasso , R2 Score : 0.9347652476618369
+
+# ====================================================================================
 
